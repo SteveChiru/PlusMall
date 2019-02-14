98c82d94710023e842927c2a9f44eff75b3a4252
SteveChiru <cqr151623@163.com>
2019-02-14 10:06:09 +0800

    module specification finishedd

--- plusmall-operator-web/src/main/webapp/js/controller/specController.js
+++ plusmall-operator-web/src/main/webapp/js/controller/specController.js
@@ -26,4 +26,44 @@ app.controller('specController',function ($scope, $controller, specService) {
             )
         }
     }
+
+    //新建
+    $scope.specPojo={specOptionsList:[]};
+    //新增选项行
+    $scope.addTableRow=function () {
+        $scope.specPojo.specOptionsList.push({});
+    }
+    //批量选项删除
+    $scope.deleTableRow=function (index) {
+        $scope.specPojo.specOptionsList.splice(index,1);//删除
+    }
+    //保存
+    $scope.save=function () {
+        var serviceObject;
+        if ($scope.brand.id != null){
+            serviceObject = specService.update($scope.specPojo);
+        }else {
+            serviceObject = specService.add($scope.specPojo);
+        }
+        serviceObject.success(
+            function (callback) {
+                if (callback.success){
+                    $scope.reloadList();
+                } else {
+                    alert(callback.msg);
+                }
+            }
+        );
+    }
+
+    //修改
+    //根据id查找单个brand的数据
+    $scope.findOne=function (id) {
+        brandService.findOne(id).success(
+            function (callback) {
+                $scope.specPojo = callback;
+            }
+        )
+    }
+
 });
