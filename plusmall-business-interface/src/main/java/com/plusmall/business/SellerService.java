3702a02bc67c79086c1717ef9b2f50f987709e2f
SteveChiru <cqr151623@163.com>
2019-02-17 22:03:00 +0800

    create plusmall-business framework

---
+++ plusmall-business-interface/src/main/java/com/plusmall/business/SellerService.java
@@ -0,0 +1,15 @@
+package com.plusmall.business;
+
+import com.plusmall.commons.PageResult;
+import com.plusmall.model.TbSeller;
+
+/**
+ * @Description:
+ */
+public interface SellerService {
+	public PageResult search(int pageNum, int pageSize, TbSeller seller) throws NullPointerException;
+	public void delete(Long[] ids) throws NullPointerException;
+	public void add(TbSeller seller) throws NullPointerException;
+	public TbSeller findOne(Long id);
+	public void update(TbSeller seller) throws NullPointerException;
+}
