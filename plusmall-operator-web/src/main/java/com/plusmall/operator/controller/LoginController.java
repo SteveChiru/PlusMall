75760441190e2a6a2ed11e231e9a3cbb9679fc9e
SteveChiru <cqr151623@163.com>
2019-02-17 15:36:20 +0800

    spring-security login module test ok

---
+++ plusmall-operator-web/src/main/java/com/plusmall/operator/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.plusmall.operator.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description:
+ */
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+	@RequestMapping("/name")
+	public Map getName(){
+		String name = SecurityContextHolder.getContext().getAuthentication().getName();
+		Map map = new HashMap();
+		map.put("loginName",name);
+		return map;
+	}
+}
