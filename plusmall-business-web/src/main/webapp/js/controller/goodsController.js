1a7346a8a25ad7f77977a32b04faecf4298bc250
SteveChiru <cqr151623@163.com>
2019-02-20 21:05:44 +0800

    kindEditor test ok

--- plusmall-business-web/src/main/webapp/js/controller/goodsController.js
+++ plusmall-business-web/src/main/webapp/js/controller/goodsController.js
@@ -3,11 +3,13 @@ app.controller('goodsController',function ($scope, $controller, goodsService) {
 
     //保存
     $scope.save=function () {
+        $scope.goodsGroup.tbGoodsDesc.introduction=editor.html();
         goodsService.add($scope.goodsGroup).success(
             function (callback) {
                 if (callback.success){
                     alert('保存成功');
                     $scope.goodsGroup={};
+                    editor.html('');    //清空富文本编辑器
                 } else {
                     alert(callback.msg);
                 }
