0ec9066be4d349375760be02e4d72d6a207193ad
SteveChiru <cqr151623@163.com>
2019-02-20 14:47:07 +0800

    create pojogroup:Goods;mybatis-generator generate tb_goods/tb_goods_desc/tb_item

---
+++ plusmall-pojo/src/main/java/com/plusmall/pojogroup/Goods.java
@@ -0,0 +1,41 @@
+package com.plusmall.pojogroup;
+
+import com.plusmall.model.TbGoods;
+import com.plusmall.model.TbGoodsDesc;
+import com.plusmall.model.TbItem;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @Description:
+ */
+public class Goods implements Serializable {
+	private TbGoods tbGoods;		//商品SPU
+	private TbGoodsDesc tbGoodsDesc;		//商品扩展
+	private List<TbItem> itemList;	//商品SKU列表
+
+	public TbGoods getTbGoods() {
+		return tbGoods;
+	}
+
+	public void setTbGoods(TbGoods tbGoods) {
+		this.tbGoods = tbGoods;
+	}
+
+	public TbGoodsDesc getTbGoodsDesc() {
+		return tbGoodsDesc;
+	}
+
+	public void setTbGoodsDesc(TbGoodsDesc tbGoodsDesc) {
+		this.tbGoodsDesc = tbGoodsDesc;
+	}
+
+	public List<TbItem> getItemList() {
+		return itemList;
+	}
+
+	public void setItemList(List<TbItem> itemList) {
+		this.itemList = itemList;
+	}
+}
