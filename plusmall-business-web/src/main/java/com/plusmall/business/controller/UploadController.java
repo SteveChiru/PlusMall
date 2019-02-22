fa1585c6e04899e03a1bc7b6fa301a589fbb3c3d
SteveChiru <cqr151623@163.com>
2019-02-22 15:23:53 +0800

    seller:add goods module:basic goods infomation test ok

---
+++ plusmall-business-web/src/main/java/com/plusmall/business/controller/UploadController.java
@@ -0,0 +1,45 @@
+package com.plusmall.business.controller;
+
+import com.plusmall.commons.ActionResult;
+import com.plusmall.commons.FastDFSClient;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @Description:
+ */
+@RestController
+public class UploadController {
+	private static Logger logger = Logger.getLogger(UploadController.class);
+	private static String logStr = "进入UploadController-";
+	private static ActionResult actionResult;
+
+	@Value("${FILE_SERVER_URL}")
+	private String FILE_SERVER_URL;	//文件服务器地址
+
+	@RequestMapping("/upload")
+	public ActionResult upload(MultipartFile file){
+		logger.info(logStr+"upload方法");
+		//1.取文件的扩展名
+		String originalFilename = file.getOriginalFilename();
+		logger.info(originalFilename);
+		String extName = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+		logger.info(extName);
+		try {
+			//2.创建一个FastDFS的客户端
+			FastDFSClient fastDFSClient = new FastDFSClient("classpath:fdfs_client.conf");
+			//3.执行上传处理
+			String path = fastDFSClient.uploadFile(file.getBytes(), extName);
+			//4.拼接返回的url和ip地址，拼装成完整的url
+			String url = FILE_SERVER_URL + path;
+			actionResult = new ActionResult(true,url);
+		}catch (Exception e){
+			e.printStackTrace();
+			actionResult = new ActionResult(false,"上传失败");
+		}
+		return actionResult;
+	}
+}
--- plusmall-business-web/src/main/webapp/js/controller/goodsController.js
+++ plusmall-business-web/src/main/webapp/js/controller/goodsController.js
@@ -1,4 +1,5 @@
-app.controller('goodsController',function ($scope, $controller, goodsService,itemCatService,typeTemplateService) {
+app.controller('goodsController',function ($scope, $controller,
+                   goodsService,itemCatService,typeTemplateService,uploadService) {
     $controller('baseController',{$scope:$scope});
 
     //保存
@@ -18,50 +19,83 @@ app.controller('goodsController',function ($scope, $controller, goodsService,ite
     }
 
     //读取一级分类
+    var getItemCat1List=false;
     $scope.selectItemCat1List=function () {
         itemCatService.searchByParentId(0).success(
             function (callback) {
                 $scope.itemCat1List=callback.result;
+                getItemCat1List = true;
             }
         );
     }
 
     //读取二级分类
+    var getItemCat2List = false;
     $scope.$watch('goodsGroup.tbGoods.category1Id',function (newValue, oldValue) {
         //根据选择的值，查询二级分类
-        itemCatService.searchByParentId(newValue).success(
-            function (callback) {
-                $scope.itemCat2List = callback.result;
-            }
-        );
+        if (getItemCat1List){   //只有在一级分类列表已经获取完的前提下，观察一分类变化，才搜寻二级分类
+            itemCatService.searchByParentId(newValue).success(
+                function (callback) {
+                    $scope.itemCat2List = callback.result;
+                    getItemCat2List = true;
+                }
+            );
+        }
     });
 
     //读取三级分类
+    var getItemCat3List = false;
     $scope.$watch('goodsGroup.tbGoods.category2Id',function (newValue, oldValue) {
         //根据选择的值，查询二级分类
-        itemCatService.searchByParentId(newValue).success(
-            function (callback) {
-                $scope.itemCat3List=callback.result;
-            }
-        );
+        if (getItemCat2List){
+            itemCatService.searchByParentId(newValue).success(
+                function (callback) {
+                    $scope.itemCat3List=callback.result;
+                    getItemCat3List = true;
+                }
+            );
+        }
     });
 
     //三级分类选择后 读取模板ID
+    var getTypeTemplate = false;
     $scope.$watch('goodsGroup.tbGoods.category3Id',function (newValue, oldValue) {
-        itemCatService.findOne(newValue).success(
-            function (callback) {
-                $scope.goodsGroup.tbGoods.typeTemplateId = callback.typeId;     //更新模板ID
-            }
-        )
+        if (getItemCat3List){
+            itemCatService.findOne(newValue).success(
+                function (callback) {
+                    $scope.goodsGroup.tbGoods.typeTemplateId = callback.typeId;     //更新模板ID
+                    getTypeTemplate = true;
+                }
+            )
+        }
+
     })
 
     //模板ID选择后，更新品牌列表
     $scope.$watch('goodsGroup.tbGoods.typeTemplateId',function (newValue, oldValue) {
-        typeTemplateService.findOne(newValue).success(
-            function (callback) {
-                $scope.typeTemplate=callback;
-                $scope.typeTemplate.brandIds=JSON.parse($scope.typeTemplate.brandIds);//品牌列表
-            }
-        )
+        if (getTypeTemplate){
+            typeTemplateService.findOne(newValue).success(
+                function (callback) {
+                    $scope.typeTemplate=callback;
+                    $scope.typeTemplate.brandIds=JSON.parse($scope.typeTemplate.brandIds);//品牌列表
+                }
+            );
+        }
     })
+
+    //上传图片
+    $scope.uploadFile=function () {
+        uploadService.uploadFile().success(
+            function (callback) {
+                if (callback.success){  //如果上传成功，取出url
+                    $scope.imageUpload.url=callback.msg;   //设置文件地址
+                    alert("上传成功，图片存储路径为："+callback.msg);
+                } else {
+                    alert(callback.msg);
+                }
+            }
+        ).error(function () {
+            alert("上传发生错误");
+        });
+    }
 })
