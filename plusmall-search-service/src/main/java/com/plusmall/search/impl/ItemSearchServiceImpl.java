ee016160dfb63d1e201de7bdb58792cfb42c2409
SteveChiru <cqr151623@163.com>
2019-03-14 10:18:42 +0800

    search:highlight test ok

--- plusmall-search-service/src/main/java/com/plusmall/search/impl/ItemSearchServiceImpl.java
+++ plusmall-search-service/src/main/java/com/plusmall/search/impl/ItemSearchServiceImpl.java
@@ -6,9 +6,9 @@ import com.plusmall.search.ItemSearchService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.Query;
-import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 
 import java.util.HashMap;
@@ -29,13 +29,24 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 	public Map<String, Object> search(Map searchMap) throws NullPointerException {
 		logger.info(logStr+"search方法");
 		Map<String,Object> map = new HashMap<>();
-		Query query = new SimpleQuery();
+		HighlightQuery highlightQuery = new SimpleHighlightQuery();
+		HighlightOptions highlightOptions = new HighlightOptions();
+		highlightOptions.addField("item_title");	//设置高亮的域
+		highlightOptions.setSimplePrefix("<em style='color:red'>");//高亮前缀
+		highlightOptions.setSimplePostfix("</em>");//高亮后缀
+		highlightQuery.setHighlightOptions(highlightOptions);	//设置高亮选项
+
 		//添加查询条件
 		Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
-		query.addCriteria(criteria);
-		ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
+		highlightQuery.addCriteria(criteria);
+		HighlightPage<TbItem> page = solrTemplate.queryForHighlightPage(highlightQuery, TbItem.class);
+		for (HighlightEntry<TbItem> h : page.getHighlighted()){	//循环高亮入口集合
+			TbItem item = h.getEntity();
+			if(h.getHighlights().size()>0 && h.getHighlights().get(0).getSnipplets().size()>0){
+				item.setTitle(h.getHighlights().get(0).getSnipplets().get(0));//设置高亮的结果
+			}
+		}
 		map.put("rows",page.getContent());
-
 		return map;
 	}
 }
